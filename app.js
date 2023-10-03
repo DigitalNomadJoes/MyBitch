3f5ce7cc9cd352c40774e280cf07638c4aa053aa
Clayton <cltnjbatchelor@gmail.com>
2023-10-03 12:22:57 -0700

    Creating my first application My Bitch

---
+++ app.js
@@ -0,0 +1,5 @@
+// ID: 1158733932020367400
+// TOKEN: MTE1ODczMzkzMjAyMDM2NzQwMA.GaLgi8.t3qO-Z7OifUi9qdMeLuiptlQl1ijJfB_Dq17gA
+// Invite Link: https://discord.com/api/oauth2/authorize?client_id=1158733932020367400&permissions=8&scope=bot
+
+
